f7daf87a297639d84daeba307ea502e0eccdaa1a
Kristian Evensen <kristian.evensen@gmail.com>
2013-10-28 12:05:13 +0100

    Added some new headers

--- qmi_hdrs.h
+++ qmi_hdrs.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+//qmux header
 struct qmux_header{
     //This value (I/F type) is not really part of the qmux header, but keep it
     //here for convenience
@@ -13,6 +14,31 @@ struct qmux_header{
     uint8_t client_id;
 } __attribute__((packed));
 
+//The two different types of QMI headers I have seen. According to the
+//specification, the size of the header is implementation-dependant (depending
+//on service)
+struct qmi_header_ctl{
+    uint8_t control_flags;
+    uint8_t transaction_id;
+    uint16_t message_id;
+    uint16_t length;
+} __attribute__((packed));
+
+struct qmi_header_gen{
+    uint8_t control_flags;
+    uint16_t transaction_id;
+    uint16_t message_id;
+    uint16_t length;
+} __attribute__((packed));
+
+struct qmi_tlv{
+    uint8_t type;
+    uint16_t length;
+} __attribute__((packed));
+
 typedef struct qmux_header qmux_header_t;
+typedef struct qmi_header_ctl qmi_header_ctl_t;
+typedef struct qmi_header_gen qmi_header_gen_t;
+typedef struct qmi_tlv qmi_tlv_t;
 
 #endif
